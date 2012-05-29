ea6896fab0da1e3373b7d2e57775ff22b5919343
Buzgau Mihai <mihai.buzgau@uhurusoftware.com>
2012-05-29 13:58:17 +0300

    windows network settup

---
+++ src/Uhuru.BOSH.Agent/Platforms/Windows/WindowsNetwork.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+
+namespace Uhuru.BOSH.Agent.Platforms.Windows
+{
+    public class WindowsNetwork
+    {
+        public WindowsNetwork()
+        {
+
+        }
+
+        public void SetupNetwork()
+        {
+            
+            //List<string> macAddresses = GetMacAddresses().ToList();
+
+            SetIP(string.Empty, string.Empty, string.Empty);
+
+        }
+
+        //private Collection<string> GetMacAddresses()
+        //{   
+        //    Collection<string> macAddresses = new Collection<string>();
+
+        //    foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+        //    {
+        //        macAddresses.Add(nic.GetPhysicalAddress().ToString());
+        //    }
+        //    return macAddresses;
+        //}
+
+
+        private void SetIP(string ipAddress, string subnetMask, string macAddress)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["MACAddress"] == macAddress)
+                    {
+                        try
+                        {
+                            ManagementBaseObject setIP;
+                            ManagementBaseObject newIP =
+                                objMO.GetMethodParameters("EnableStatic");
+
+                            newIP["IPAddress"] = new string[] { ipAddress };
+                            newIP["SubnetMask"] = new string[] { subnetMask };
+
+                            // setIP = objMO.InvokeMethod("EnableStatic", newIP, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set's a new Gateway address of the local machine
+        /// </summary>
+        /// <param name="gateway">The Gateway IP Address</param>
+        /// <remarks>Requires a reference to the System.Management namespace</remarks>
+        private void SetGateway(string gateway, string macAddress)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["MACAddress"] == macAddress)
+                    {
+                        try
+                        {
+                            ManagementBaseObject setGateway;
+                            ManagementBaseObject newGateway =
+                                objMO.GetMethodParameters("SetGateways");
+
+                            newGateway["DefaultIPGateway"] = new string[] { gateway };
+                            newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                            setGateway = objMO.InvokeMethod("SetGateways", newGateway, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set's the DNS Server of the local machine
+        /// </summary>
+        /// <param name="NIC">NIC address</param>
+        /// <param name="DNS">DNS server address</param>
+        /// <remarks>Requires a reference to the System.Management namespace</remarks>
+        private void SetDNS(string DNS, string macAddress)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["MACAddress"].Equals(macAddress))
+                    {
+                        try
+                        {
+                            ManagementBaseObject newDNS =
+                                objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                            newDNS["DNSServerSearchOrder"] = DNS.Split(',');
+                            ManagementBaseObject setDNS =
+                                objMO.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Set's WINS of the local machine
+        /// </summary>
+        /// <param name="NIC">NIC Address</param>
+        /// <param name="priWINS">Primary WINS server address</param>
+        /// <param name="secWINS">Secondary WINS server address</param>
+        /// <remarks>Requires a reference to the System.Management namespace</remarks>
+        private void SetWINS(string priWINS, string secWINS, string macAddress)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["MACAddress"].Equals(macAddress))
+                    {
+                        try
+                        {
+                            ManagementBaseObject setWINS;
+                            ManagementBaseObject wins =
+                            objMO.GetMethodParameters("SetWINSServer");
+                            wins.SetPropertyValue("WINSPrimaryServer", priWINS);
+                            wins.SetPropertyValue("WINSSecondaryServer", secWINS);
+
+                            setWINS = objMO.InvokeMethod("SetWINSServer", wins, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Uhuru.BOSH.Test/Unit/WindowsNetworkTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uhuru.BOSH.Agent.Platforms.Windows;
+
+namespace Uhuru.BOSH.Test.Unit
+{
+    [TestClass]
+    public class WindowsNetworkTest
+    {
+        [TestMethod]
+        public void TC001_TestNetwork()
+        {
+            WindowsNetwork wn = new WindowsNetwork();
+            wn.SetupNetwork();
+        }
+    }
+}
