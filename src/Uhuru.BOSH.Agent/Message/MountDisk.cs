8a5a6ef0b5fafd7d4e8de8cf36475bf356789fb0
florindragos <florind@uhurusoftware.com>
2012-05-31 12:19:37 +0300

    Implemented MountDisk moved DiskUtils methods to DiskUtil

--- src/Uhuru.BOSH.Agent/Message/MountDisk.cs
+++ src/Uhuru.BOSH.Agent/Message/MountDisk.cs
@@ -10,106 +10,103 @@ namespace Uhuru.BOSH.Agent.Message
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Uhuru.Utilities;
+    using System.IO;
+    using Uhuru.BOSH.Agent.Errors;
 
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    public class MountDisk
+    public class MountDisk : Base
     {
-      ////       def self.process(args)
-      ////  new(args).mount
-      ////end
+        int cid;
 
-      ////def initialize(args)
-      ////  @cid = args.first
-      ////end
+        public static void Process(dynamic[] args)
+        {
+            new MountDisk(args).Mount();
+        }
 
-      ////def mount
-      ////  if Bosh::Agent::Config.configure
-      ////    update_settings
-      ////    logger.info("MountDisk: #{@cid} - #{settings['disks'].inspect}")
+        public MountDisk(dynamic[] args)
+        {
+            cid = args[0];
+        }
 
-      ////    setup_disk
-      ////  end
-      ////end
+        public void Mount()
+        {
+            if (!string.IsNullOrEmpty(Config.Configure))
+            {
+                UpdateSettings();
+                Logger.Info("MountDisk: {0} - {1}", cid, Settings["disk"].ToString());
+
+                SetupDisk();
+            }
+        }
+
+        public void UpdateSettings()
+        {
+            // TODO: Bosh::Agent::Config.settings = Bosh::Agent::Config.infrastructure.load_settings
+            Logger.Info(String.Format("Settings: {0}", Settings.ToString()));
+            throw new NotImplementedException();
+        }
 
       ////def update_settings
       ////  Bosh::Agent::Config.settings = Bosh::Agent::Config.infrastructure.load_settings
       ////  logger.info("Settings: #{settings}")
       ////end
 
-      ////def setup_disk
-      ////  disk = Bosh::Agent::Config.platform.lookup_disk_by_cid(@cid)
-      ////  partition = "#{disk}1"
+        public void SetupDisk()
+        {
+            Logger.Info(String.Format("Setup disk settings: {0}", Settings.ToString()));
 
-      ////  logger.info("setup disk settings: #{settings.inspect}")
+            if (!DiskUtil.DiskHasPartition(cid))
+            {
+                Logger.Info(String.Format("Found blank disk ", cid));
+                int returnCode = DiskUtil.CreatePrimaryPartition(cid, "store");
+                if (returnCode != 0)
+                {
+                    throw new MessageHandlerException(String.Format("Unable to create partition. Exit code: {0}", returnCode));
+                }
+            }
+            else
+            {
+                Logger.Info(String.Format("Disk has partition"));
+            }
 
-      ////  read_disk_attempts = 300
-      ////  read_disk_attempts.downto(0) do |n|
-      ////    begin
-      ////      # Parition table is blank
-      ////      disk_data = File.read(disk, 512)
+            MountPersistentDisk(cid);
+        }
 
-      ////      if disk_data == "\x00"*512
-      ////        logger.info("Found blank disk #{disk}")
-      ////      else
-      ////        logger.info("Disk has partition table")
-      ////        logger.info(`sfdisk -Llq #{disk} 2> /dev/null`)
-      ////      end
-      ////      break
-      ////    rescue => e
-      ////      # Do nothing - we'll retry
-      ////      logger.info("Re-trying reading from #{disk}")
-      ////    end
+        public void MountPersistentDisk(int diskIndex)
+        {
+            string storeMountPoint = Path.Combine(BaseDir, "store");
+            string mountpoint;
 
-      ////    if n == 0
-      ////      raise Bosh::Agent::MessageHandlerError, "Unable to read from new disk"
-      ////    end
-      ////    sleep 1
-      ////  end
+            if (DiskUtil.IsMountPoint(storeMountPoint))
+            {
+                Logger.Info("Mounting persistent disk store migration target");
+                mountpoint = Path.Combine(BaseDir, "store_migraton_target");
+            }
+            else
+            {
+                Logger.Info("Mounting persistent disk store");
+                mountpoint = storeMountPoint;
+            }
 
-      ////  if File.blockdev?(disk) && DiskUtil.ensure_no_partition?(disk, partition)
-      ////    full_disk = ",,L\n"
-      ////    logger.info("Partitioning #{disk}")
+            if (!Directory.Exists(mountpoint))
+            {
+                Directory.CreateDirectory(mountpoint);
+            }
 
-      ////    Bosh::Agent::Util.partition_disk(disk, full_disk)
+            Logger.Info(String.Format("Mount Partition {0} {1}", diskIndex, mountpoint));
 
-      ////    `/sbin/mke2fs -t ext4 -j #{partition}`
-      ////    unless $?.exitstatus == 0
-      ////      raise Bosh::Agent::MessageHandlerError, "Failed create file system (#{$?.exitstatus})"
-      ////    end
-      ////  elsif File.blockdev?(partition)
-      ////    logger.info("Found existing partition on #{disk}")
-      ////    # Do nothing
-      ////  else
-      ////    raise Bosh::Agent::MessageHandlerError, "Unable to format #{disk}"
-      ////  end
+            int returnCode = DiskUtil.MountPartition(diskIndex, mountpoint);
 
-      ////  mount_persistent_disk(partition)
-      ////  {}
-      ////end
+            if (returnCode != 0)
+            {
+                throw new MessageHandlerException(String.Format("Failed mount disk {0} on {1}. Exit code: {2}", diskIndex, mountpoint, returnCode));
+            }
 
-      ////def mount_persistent_disk(partition)
-      ////  store_mountpoint = File.join(base_dir, 'store')
+        }
 
-      ////  if Pathname.new(store_mountpoint).mountpoint?
-      ////    logger.info("Mounting persistent disk store migration target")
-      ////    mountpoint = File.join(base_dir, 'store_migraton_target')
-      ////  else
-      ////    logger.info("Mounting persistent disk store")
-      ////    mountpoint = store_mountpoint
-      ////  end
-
-      ////  FileUtils.mkdir_p(mountpoint)
-      ////  FileUtils.chmod(0700, mountpoint)
-
-      ////  logger.info("Mount #{partition} #{mountpoint}")
-      ////  `mount #{partition} #{mountpoint}`
-      ////  unless $?.exitstatus == 0
-      ////    raise Bosh::Agent::MessageHandlerError, "Failed mount #{partition} on #{mountpoint} #{$?.exitstatus}"
-      ////  end
-      ////end
-
-      ////def self.long_running?; true; end
+        public static bool LongRunning { get { return true; } }
     }
 }
