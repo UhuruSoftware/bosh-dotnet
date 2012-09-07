1b51b3eba9dbe0a5717c219f37e68172a25ce6c2
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2012-09-07 11:53:38 +0300

    UH-1151 tests.
    Change-Id: I188f15decbebe7df9f8e3fb2d3a40da0173ceb37

--- src/Uhuru.BOSH.Test/Integration/CompilePackageTest.cs
+++ src/Uhuru.BOSH.Test/Integration/CompilePackageTest.cs
@@ -54,14 +54,23 @@ namespace Uhuru.BOSH.Test.Unit
 
             string requestMessage = "{\"method\":\"compile_package\",\"arguments\":[\""+packageId+"\",\"60209d197f9f93a338c93af3228739cdfbd38992\",\"postgresql\",\"5.1\",{}],\"reply_to\":\"director.6d811790-5ceb-4251-93d8-5efa8bbf3bac.5de47a59-21ce-4db6-a7b9-5012a6cdf3bb\"}";
 
+            ManualResetEvent resposeReceived = new ManualResetEvent(false);
+            string response = null;
+
             var natsClient = NatsClient.ReactorFactory.GetReactor(typeof(NatsClient.Reactor));
             natsClient.Start(Config.MessageBus);
+            
+            natsClient.Subscribe("director.6d811790-5ceb-4251-93d8-5efa8bbf3bac.5de47a59-21ce-4db6-a7b9-5012a6cdf3bb", delegate(string msg, string reply, string subject)
+            {
+                response = msg;
+                resposeReceived.Set();
+            });
+            
             natsClient.Publish("agent.07dcbefe-e4fc-48b9-97c1-1752d5c1b6df", new SimpleCallback(MessageCallback), requestMessage);
 
-            while (true)
-            {
-                Thread.Sleep(1000);
-            }
+            resposeReceived.WaitOne(4000);
+
+            Assert.AreNotEqual(response, null);
 
             bclient.Delete(packageId);
         }
