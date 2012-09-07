9e8529bb508cb1fdfdc46c6a4b5907d21892b52c
florindragos <florind@uhurusoftware.com>
2012-09-07 15:07:24 +0300

    UH-1192
    Change-Id: I93bcf2870665d4cf152c2af2f9ea948a79f3bd3e

--- src/Uhuru.BOSH.Agent/Infrastructures/VSphere/Settings.cs
+++ src/Uhuru.BOSH.Agent/Infrastructures/VSphere/Settings.cs
@@ -10,12 +10,29 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Uhuru.Utilities;
+    using Newtonsoft.Json;
+    using System.IO;
+    using System.Management;
+    using Uhuru.BOSH.Agent.Errors;
+    using System.Diagnostics;
+    using System.Threading;
 
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
     public class Settings
     {
+        string CdromSettingsMountPoint
+        {
+            get
+            {
+                return Path.Combine(Config.BaseDir, "bosh", "settings");
+            }
+        }
+     
+
+
     ////def initialize
     ////  logger                      = Bosh::Agent::Config.logger
     ////  base_dir                    = Bosh::Agent::Config.base_dir
@@ -27,7 +44,7 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
         /// </summary>
         public Settings()
         {
-            
+
         }
 
     ////def load_settings
@@ -42,9 +59,20 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
     ////  end
     ////  Bosh::Agent::Config.settings = @settings
     ////end
-        private void LoadSettings()
+        public dynamic LoadSettings()
         {
+            Logger.Info("Loading vsphere settings");
+            LoadCDRomSettings();
 
+            Logger.Info("Loading settings file");
+            //YamlNode root = null;
+            string fileContent = File.ReadAllText(Config.SettingsFile);
+            //using (TextReader textReader = new StreamReader(Config.SettingsFile))
+            //{
+            //    YamlNode[] nodes = YamlNode.FromYaml(textReader);
+            //    root = nodes[0];
+            //}
+            return JsonConvert.DeserializeObject(fileContent);
         }
 
     ////def load_cdrom_settings
@@ -67,11 +95,6 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
     ////  end
     ////end
 
-        private void LoadCDRomSettings()
-        {
-
-        }
-
     ////def check_cdrom
     ////  begin
     ////    File.read('/dev/cdrom', 0)
@@ -89,11 +112,58 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
     ////  FileUtils.chmod(0700, @cdrom_settings_mount_point)
     ////end
 
+        private void CreateMountPointDirectory()
+        {
+            if (!Directory.Exists(CdromSettingsMountPoint))
+            {
+                Directory.CreateDirectory(CdromSettingsMountPoint);
+            }
+        }
+
     ////def mount_cdrom
     ////  output = `mount /dev/cdrom #{@cdrom_settings_mount_point} 2>&1`
     ////  raise Bosh::Agent::LoadSettingsError,
     ////    "Failed to mount settings on #{@cdrom_settings_mount_point}: #{output}" unless $?.exitstatus == 0
     ////end
+
+        private void MountCDROM(string volumeName)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "mountvol.exe";
+            info.Arguments = String.Format("{0} {1}", CdromSettingsMountPoint, volumeName);
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+
+            int retryCount = 10;
+            while (retryCount > 0)
+            {
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForExit(60000);
+                if (!p.HasExited)
+                {
+                    p.Kill();
+                    break;
+                }
+                else
+                {
+                    Logger.Debug(p.StandardOutput.ReadToEnd());
+                    if (p.ExitCode != 0)
+                    {
+                        retryCount--;
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            throw new LoadSettingsException("Failed to mount settings on: " + CdromSettingsMountPoint);
+        }
 
     ////def load_settings_file(settings_file)
     ////  if File.exists?(settings_file)
@@ -108,8 +178,110 @@ namespace Uhuru.BOSH.Agent.Infrastructures.VSphere
     ////  `umount #{@cdrom_settings_mount_point} 2>&1`
     ////end
 
+        private void UnmountCDROM(string volumeName)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "mountvol.exe";
+            info.Arguments = String.Format("{0} /D", CdromSettingsMountPoint);
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+
+            int retryCount = 10;
+            while (retryCount > 0)
+            {
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForExit(60000);
+                if (!p.HasExited)
+                {
+                    p.Kill();
+                    break;
+                }
+                else
+                {
+                    Logger.Debug(p.StandardOutput.ReadToEnd());
+                    if (p.ExitCode != 0)
+                    {
+                        retryCount--;
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            throw new LoadSettingsException("Failed to mount settings on: " + CdromSettingsMountPoint);
+        }
+
     ////def eject_cdrom
     ////  `eject /dev/cdrom`
     ////end
+
+        private void EjectCDROM()
+        {
+        }
+
+        private void LoadCDRomSettings()
+        {
+            Logger.Info("Loading cdrom settings");
+
+            string volumeName = GetCdRomVolumeName();
+
+            if (volumeName.Equals(string.Empty, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new LoadSettingsException("No bosh cdrom env");
+            }
+
+            CreateMountPointDirectory();
+            MountCDROM(volumeName);
+
+            string envPath = Path.Combine(CdromSettingsMountPoint, "ENV");
+
+            try
+            {
+                if (File.Exists(Config.SettingsFile))
+                {
+                    File.Delete(Config.SettingsFile);
+                }
+                File.Copy(envPath, Config.SettingsFile, true);
+
+                File.SetAttributes(Config.SettingsFile, FileAttributes.Normal);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.ToString());
+                throw new LoadSettingsException("Failed to read/write env/settings.json");
+            }
+            finally
+            {
+                UnmountCDROM(volumeName);
+            }
+        }
+
+        private string GetCdRomVolumeName()
+        {
+            string volumeName = string.Empty;
+            using (ManagementClass volume = new ManagementClass("Win32_Volume"))
+            {
+                ManagementObjectCollection moc = volume.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    if (mo["DriveType"] != null)
+                    {
+                        if (mo["DriveType"].ToString().Equals("5", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            volumeName = mo["DeviceID"].ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            return volumeName;
+        }
+
     }
 }
--- src/Uhuru.BOSH.Agent/Infrastructures/VSphereInfrastructure.cs
+++ src/Uhuru.BOSH.Agent/Infrastructures/VSphereInfrastructure.cs
@@ -14,6 +14,8 @@ namespace Uhuru.BOSH.Agent.Infrastructures
     using Uhuru.Utilities;
     using System.IO;
     using Newtonsoft.Json;
+    using System.Management;
+    using Uhuru.BOSH.Agent.Infrastructures.VSphere;
     //using System.Yaml;
 
     /// <summary>
@@ -26,18 +28,7 @@ namespace Uhuru.BOSH.Agent.Infrastructures
         /// </summary>
         public dynamic LoadSettings()
         {
-            Logger.Info("Loading vsphere settings");
-            LoadCdromSettings();
-
-            Logger.Info("Loading settings file");
-            //YamlNode root = null;
-            string fileContent = File.ReadAllText(Config.SettingsFile);
-            //using (TextReader textReader = new StreamReader(Config.SettingsFile))
-            //{
-            //    YamlNode[] nodes = YamlNode.FromYaml(textReader);
-            //    root = nodes[0];
-            //}
-            return JsonConvert.DeserializeObject(fileContent);
+            return new Settings().LoadSettings();
         }
 
         /// <summary>
@@ -50,31 +41,6 @@ namespace Uhuru.BOSH.Agent.Infrastructures
             return string.Empty;
         }
 
-        private void LoadCdromSettings()
-        {
-            Logger.Info("Loading cdrom settings");
-            foreach (DriveInfo driveInfo in DriveInfo.GetDrives())
-            {
-                if (driveInfo.DriveType == DriveType.CDRom)
-                {
-                    Logger.Info("Found cdrom at " + driveInfo.Name);
-                    Logger.Info("Checking ENV file in " + driveInfo.Name);
-                    if (File.Exists(driveInfo.Name + "ENV"))
-                    {
-                        Logger.Info("Found ENV file in " + driveInfo.Name);
-                        if (File.Exists(Config.SettingsFile))
-                        {
-                            File.Delete(Config.SettingsFile);
-                        }
-                        File.Copy(driveInfo.Name + "ENV", Config.SettingsFile, true);
-                        
-                        File.SetAttributes(Config.SettingsFile, FileAttributes.Normal);
-
-
-                    }
-                }
-            }
-
-        }
+        
     }
 }
