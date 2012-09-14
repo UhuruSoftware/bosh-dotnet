72b76df44bc509b7aab2b1ccc3395f744a4c1e9d
mihaibuzgau <mihai.buzgau@uhurusoftware.com>
2012-09-14 16:00:14 +0300

    UH-1200 and UH-1202
    Change-Id: Ieb3b6696a05a394d13302ba9dc7fd1a2ee7fdd4e

--- src/Uhuru.BOSH.Agent/Message/Drain.cs
+++ src/Uhuru.BOSH.Agent/Message/Drain.cs
@@ -14,6 +14,9 @@ using Uhuru.NatsClient;
     using Uhuru.Utilities;
     using Uhuru.BOSH.Agent.Errors;
     using System.Threading;
+    using System.Globalization;
+    using System.IO;
+    using System.Diagnostics;
 
     /// <summary>
     /// TODO: Update summary.
@@ -37,17 +40,17 @@ using Uhuru.NatsClient;
         /// </summary>
         public Drain()
         {
-            this.monit = Monit.GetInstance();
+            //this.monit = Monit.GetInstance();
         }
         
         Reactor nats;
         string agentId;
-        //TODO: Jira UH-1202
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         Agent.State oldSpec;        
         string drainType;
         dynamic spec;
-        Monit monit = null;
+        private string drainFile;
 
         /// <summary>
         /// Processes the specified args.
@@ -59,47 +62,39 @@ using Uhuru.NatsClient;
             this.nats = Config.Nats;
             this.agentId = Config.AgentId;
             this.oldSpec = Config.State;
-            drainType = args[0].Value;
+            this.drainType = args[0].Value;
+            this.drainFile = DrainScriptFile();
+
             if (args.Count > 1)
             {
                 spec = args[1];
             }
 
-
             Logger.Info("Draining " + agentId);
 
-            if (Config.Configure)
-            {
-                //TODO unmonitor service
-            }
 
             switch (drainType)
             {
                 case "shutdown": return DrainForShutdown();
-                    break;
                 case "update": return DrainForUpdate();
-                    break;
                 case "status": return DrainCheckStatus();
-                    break;
                 default: throw new MessageHandlerException("Unknown drain type " + drainType);
 
             }
-            //return string.Empty;
 
         }
 
         private object DrainForShutdown()
         {
             nats.Publish("hm.agent.shutdown." + agentId);
-            return this.RunDrainScript();
-        }
 
-        private object RunDrainScript()
-        {
-            object result =this.monit.RunPrescripts(false);
-            
-            return result;
-            //return "0";
+            if (oldSpec.Job != null && DrainScriptExists())
+            {
+                return RunDrainScript("job_shutdown", "hash_unchanged", new string[0]);
+
+            }
+            else
+                return 0;
         }
 
         private object DrainForUpdate()
@@ -108,22 +103,141 @@ using Uhuru.NatsClient;
             {
                 throw new MessageHandlerException("Drain update called without apply spec");
             }
-            spec.Remove("configuration_hash");
-            Message.Apply applyMessage = new Message.Apply();
 
-            applyMessage.Process(spec);
-
-            //Logger.Warning("Not implemented yet");
+            if (oldSpec.Job != null && DrainScriptExists())
+            {
+                return RunDrainScript(JobChange(), HashChange(), UpdatePackages());
+            }
 
             return 0;
-            
+
         }
 
-        //TODO : Jira 12000
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
-        private string DrainCheckStatus()
+        private string JobChange()
         {
-            throw new NotImplementedException();
+            if (oldSpec.Job == null)
+                return "job_new";
+            else if (oldSpec.Job.SHA1 == spec["job"]["sha1"].Value)
+                return "job_unchanged";
+            else
+                return "job_changed";
+        }
+
+        private string HashChange()
+        {
+            if (oldSpec.GetValue("configuration_hash") == null)
+            {
+                return "hash_new";
+            }
+            else if (oldSpec.GetValue("configuration_hash").Value == spec["configuration_hash"].Value)
+            {
+                return "hash_unchanged";
+            }
+            else
+                return "hash_changed";
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+        private string[] UpdatePackages()
+        {
+            List<string> result = new List<string>();
+            bool exists;
+
+            if (oldSpec.GetValue("packages") == null)
+                return result.ToArray();
+
+            //Check old packages
+            foreach (dynamic oldPkg in oldSpec.GetValue("packages"))
+            {
+            
+                foreach (dynamic newPkg in spec["packages"])
+                {
+                    if (newPkg.Name == oldPkg.Name)
+                    {
+                        if (newPkg.Value["sha1"].Value != oldPkg.Value["sha1"].Value)
+                        {
+                            result.Add(newPkg.Name);
+                        }
+                    }
+                    
+                }
+            }
+
+            //Check for new packages
+            foreach (dynamic newPkg in spec["packages"])
+            {
+                exists = false;
+                foreach (dynamic oldPkg in oldSpec.GetValue("packages"))
+                {
+                    if (newPkg.Name == oldPkg.Name)
+                    {
+                        exists = true;
+                    }
+                }
+                if (!exists)
+                    result.Add(newPkg.Name);
+            }
+            
+            return result.ToArray();
+        }
+
+        private bool DrainScriptExists()
+        {
+            if (File.Exists(this.drainFile))
+                return true;
+            else
+                return false;
+        }
+
+        private string DrainScriptFile()
+        {
+            string jobTemplate = this.oldSpec.Job.Template;
+            return string.Format(CultureInfo.InvariantCulture, @"{0}\jobs\{1}\drain.bat", Config.BaseDir, jobTemplate);
+        }
+
+        private object RunDrainScript(string jobUpdated, string hashUpdated, string[] updatedPackages)
+        {
+            int result =0;
+            string standardOutput = string.Empty;
+            string standardError = string.Empty;
+            Logger.Info(string.Format(CultureInfo.InvariantCulture, "Running drain script with job updated: {0}, hash updated: {1} and updated packages {2}", jobUpdated, hashUpdated, string.Join(" ", updatedPackages)));
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = drainFile;
+            info.EnvironmentVariables.Add("job_change", jobUpdated);
+            info.EnvironmentVariables.Add("hash_change", hashUpdated);
+            info.EnvironmentVariables.Add("updated_packages", String.Join(" ", updatedPackages));
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            using (System.Diagnostics.Process p = new Process())
+            {
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForExit();
+                result = p.ExitCode;
+                standardOutput = p.StandardOutput.ReadToEnd();
+                standardError = p.StandardError.ReadToEnd();
+            }
+            if (!String.IsNullOrEmpty(standardError))
+            {
+                string errorMessage = string.Format(CultureInfo.InvariantCulture, "Drain script error exit {0} : {1}", result, standardError);
+                MessageHandlerException massageHandlerException = new MessageHandlerException(errorMessage);
+                Logger.Error(errorMessage);
+                throw massageHandlerException;
+            }
+            else
+            {
+                Logger.Info(string.Format(CultureInfo.InvariantCulture, "Finished running drain script, output :{0}", standardOutput));
+            }
+
+            return result;
+        }
+        
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        private object DrainCheckStatus()
+        {
+            return RunDrainScript("job_check_status", "hash_unchanged", new string[0]);
         }
         
     }
---
+++ src/Uhuru.BOSH.Test/Integration/DrainTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Uhuru.BOSH.Agent;
+using System.Threading;
+using Uhuru.BOSH.Agent.Message;
+using Newtonsoft.Json;
+
+namespace Uhuru.BOSH.Test.Integration
+{
+    [TestClass]
+    [DeploymentItem("log4net.config")]
+    [DeploymentItem("unity.config")]
+    public class DrainTest
+    {
+        private static string configFile = @"C:\vcap\bosh\settings.json";
+
+        [TestMethod]
+        public void TC001_TestDrainStatus()
+        {
+            //Arrange
+            string fileContent = File.ReadAllText(configFile);
+            Config.Setup(JsonConvert.DeserializeObject(fileContent), false);
+            string argsString = "[\"status\"]";
+            dynamic args = JsonConvert.DeserializeObject(argsString); 
+            //ConfigBlobStore
+            Config.Configure = true;
+            Config.BlobstoreProvider = Config.Settings["blobstore"]["plugin"].Value;
+            Config.BlobstoreOptions = Config.Settings["blobstore"]["properties"];
+            Monit.GetInstance().Start();
+
+            //Act
+            Drain drain = new Drain();
+            object drainResult = drain.Process(args);
+
+            //Assert
+            Assert.AreEqual(drainResult.ToString(), "0");
+        }
+
+        [TestMethod]
+        public void TC002_TestDrainShutdown()
+        {
+            //Arrange
+            string fileContent = File.ReadAllText(configFile);
+            Config.Setup(JsonConvert.DeserializeObject(fileContent), false);
+            string argsString = "[\"shutdown\"]";
+            dynamic args = JsonConvert.DeserializeObject(argsString);
+            //ConfigBlobStore
+            Config.Configure = true;
+            Config.BlobstoreProvider = Config.Settings["blobstore"]["plugin"].Value;
+            Config.BlobstoreOptions = Config.Settings["blobstore"]["properties"];
+            Config.Nats = new NatsClient.Reactor();
+            Uri natsUri = new Uri(Config.MessageBus);
+            Config.Nats.Start(natsUri);
+            Monit.GetInstance().Start();
+
+            //Act
+            Drain drain = new Drain();
+            object drainResult = drain.Process(args);
+
+            //Assert
+            Assert.AreEqual(drainResult.ToString(), "0");
+        }
+
+
+        [TestMethod]
+        public void TC003_TestDrainUpdate()
+        {
+            //Arrange
+            string fileContent = File.ReadAllText(configFile);
+            Config.Setup(JsonConvert.DeserializeObject(fileContent), false);
+            string argsString = "[\"update\",{\"deployment\":\"uhuru-cf-mitza\",\"release\":{\"name\":\"uhuru-cf\",\"version\":\"86.11-dev\"},\"job\":{\"name\":\"win_dea\",\"release\":\"uhuru-cf\",\"template\":\"win_dea\",\"version\":\"0.2-dev\",\"sha1\":\"cea000d7e5611f3fff6ddbf46163b6a4b025664b\",\"blobstore_id\":\"c9034ac9-ffc1-4aff-b17a-9be8533a6bfa\"},\"index\":0,\"networks\":{\"default\":{\"ip\":\"10.0.3.137\",\"netmask\":\"255.255.0.0\",\"cloud_properties\":{\"name\":\"VM Network\"},\"default\":[\"dns\",\"gateway\"],\"dns\":[\"10.0.0.10\",\"8.8.8.8\"],\"gateway\":\"10.0.0.1\"}},\"resource_pool\":{\"name\":\"windowspool\",\"cloud_properties\":{\"ram\":1000,\"cpu\":1,\"disk\":10192},\"stemcell\":{\"name\":\"test-stemcell\",\"version\":\"0.1.4\"}},\"packages\":{\"win_dea1\":{\"name\":\"win_dea\",\"version\":\"0.1-dev.1\",\"sha1\":\"8a90c0c416601e90bbe6a177d79d1efc2e47d526\",\"blobstore_id\":\"ba697254-60d5-4360-816d-e4dd5d3b8d02\"}},\"persistent_disk\":0,\"configuration_hash\":\"f7452dda13a19bc46a15d725e0151007ae194e2b\",\"properties\":{\"domain\":\"vcap.me\",\"env\":{},\"networks\":{\"apps\":\"default\",\"management\":\"default\"},\"nats\":{\"user\":\"nats\",\"password\":\"nats\",\"address\":\"10.0.3.136\",\"port\":4222},\"ccdb\":{\"user\":\"ccadmin\",\"password\":\"ccadmin\",\"address\":\"10.0.3.136\",\"port\":5524,\"dbname\":\"appcloud\",\"databases\":[{\"tag\":\"cc\",\"name\":\"appcloud\"}],\"roles\":[{\"tag\":\"admin\",\"name\":\"ccadmin\",\"password\":\"ccadmin\"}]},\"cc\":{\"srv_api_uri\":\"http://api.vcap.me\",\"password\":\"aaauuLaap44jadlas2l312lk\",\"token\":\"aaaf4eaa8c1758f66d5cb7adcb24adb9d7\",\"use_nginx\":false,\"allow_registration\":true,\"uaa\":{\"enabled\":false,\"resource_id\":\"cloud_controller\",\"client_secret\":\"aaaNxRJcx6cpv\"},\"admins\":[\"dev@cloudfoundry.org\"]},\"router\":{\"status\":{\"port\":8080,\"user\":\"vcap\",\"password\":\"vcap\"}},\"dashboard\":{\"users\":[[\"dash\",\"admin\"]]},\"dea\":{\"max_memory\":4096},\"nfs_server\":{\"address\":\"10.0.3.136\",\"network\":\"10.0.0.0/16\"},\"opentsdb\":{\"address\":\"10.0.3.136\",\"port\":4242},\"service_plans\":{\"mysql\":{\"free\":{\"job_management\":{\"high_water\":1400,\"low_water\":100},\"configuration\":{\"allow_over_provisioning\":true,\"capacity\":200,\"max_db_size\":128,\"max_long_query\":3,\"max_long_tx\":0,\"max_clients\":20}}},\"postgresql\":{\"free\":{\"job_management\":{\"high_water\":1400,\"low_water\":100},\"configuration\":{\"capacity\":200,\"max_db_size\":128,\"max_long_query\":3,\"max_long_tx\":30,\"max_clients\":20}}},\"mongodb\":{\"free\":{\"job_management\":{\"high_water\":3000,\"low_water\":100},\"configuration\":{\"allow_over_provisioning\":true,\"capacity\":200,\"quota_files\":4,\"max_clients\":500}}},\"redis\":{\"free\":{\"job_management\":{\"high_water\":1400,\"low_water\":100},\"configuration\":{\"capacity\":200,\"max_memory\":16,\"max_swap\":32,\"max_clients\":500}}},\"rabbit\":{\"free\":{\"job_management\":{\"low_water\":100,\"high_water\":1400},\"configuration\":{\"max_memory_factor\":0.5,\"max_clients\":512,\"capacity\":200}}}},\"mysql_gateway\":{\"check_orphan_interval\":7200,\"token\":\"0xfeedface\"},\"mysql_node\":{\"production\":true,\"password\":\"aaa13djkas\"},\"redis_gateway\":{\"token\":\"0xfeedface\",\"check_orphan_interval\":7200},\"redis_node\":{\"command_rename_prefix\":\"sample\"},\"mongodb_gateway\":{\"check_orphan_interval\":7200,\"token\":\"0xfeedface\"},\"mongodb_node\":{},\"uhurufs_gateway\":{\"check_orphan_interval\":7200,\"token\":\"0xfeedface\"},\"uhurufs_node\":{\"statusport\":12345,\"basedir\":\"c:\\\\vcap\\\\store\\\\uhurufs\",\"capacity\":200},\"rabbit_gateway\":{\"token\":\"AAA430A8BAED490B1240338DA34B10D\"},\"rabbit_node\":{},\"postgresql_gateway\":{\"check_orphan_interval\":7200,\"token\":\"aaaeAh4BXFBXwLrrWJCpQTfeDnaCn7m\"},\"postgresql_node\":{\"production\":true},\"uaa\":{},\"win_dea\":{\"localroute\":\"10.0.3.30\",\"fillerport\":12345}}}]";
+            dynamic args = JsonConvert.DeserializeObject(argsString);
+            //ConfigBlobStore
+            Config.Configure = true;
+            Config.BlobstoreProvider = Config.Settings["blobstore"]["plugin"].Value;
+            Config.BlobstoreOptions = Config.Settings["blobstore"]["properties"];
+            Config.Nats = new NatsClient.Reactor();
+            Uri natsUri = new Uri(Config.MessageBus);
+            Config.Nats.Start(natsUri);
+            Monit.GetInstance().Start();
+
+            //Act
+            Drain drain = new Drain();
+            object drainResult = drain.Process(args);
+
+            //Assert
+            Assert.AreEqual(drainResult.ToString(), "0");
+        }
+    }
+}
