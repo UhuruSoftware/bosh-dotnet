d2cdeb06436645e8688d42ca91516ffd85637261
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2012-09-18 16:30:50 +0300

    UH-863 Dependencies will be installed for a compilation request
    Change-Id: I58d18afba4544449a592fe3b2d9f3be61bf87f03

--- src/Uhuru.BOSH.Agent/Message/CompilePackage.cs
+++ src/Uhuru.BOSH.Agent/Message/CompilePackage.cs
@@ -18,6 +18,7 @@ namespace Uhuru.BOSH.Agent.Message
     using Uhuru.BOSH.Agent.Errors;
     using System.Diagnostics;
     using System.Globalization;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// TODO: Update summary.
@@ -145,9 +146,20 @@ namespace Uhuru.BOSH.Agent.Message
         {
             this.logger.Info("Installing dependencies");
 
-            foreach (dynamic dependency in dependencies)
+            foreach (JProperty dependency in this.dependencies)
             {
-                this.logger.Warning("TODO : install dependencies for " + dependency.ToString());
+                string packageName = dependency.Name;
+
+                this.logger.Info("Installing dependency: " + packageName + " " + dependency.ToString());
+                string blobstoreId = dependency.Value["blobstore_id"].Value<string>();
+                string sha1 = dependency.Value["sha1"].Value<string>();
+                string installDir = Path.Combine(this.installBase, packageName, dependency["version"].Value<string>());
+
+                Util.UnpackBlob(blobstoreId, sha1, installDir);
+
+                string packageLinkDestination = Path.Combine(Config.BaseDir, "packages", packageName);
+
+                Util.CreateSymLink(installDir, packageLinkDestination);
             }
         }
 
@@ -200,7 +212,6 @@ namespace Uhuru.BOSH.Agent.Message
                 Directory.Delete(this.installDir, true);
             }
             Directory.CreateDirectory(this.installDir);
-            // TODO: check if enough disk is available
 
             Alphaleonis.Win32.Filesystem.DiskSpaceInfo diskSpace = Alphaleonis.Win32.Filesystem.Volume.GetDiskFreeSpace(this.compileBase);
             double usedDiskPercent = 100.0 * (double)(diskSpace.TotalNumberOfBytes - diskSpace.TotalNumberOfFreeBytes) / (double)diskSpace.TotalNumberOfBytes;
