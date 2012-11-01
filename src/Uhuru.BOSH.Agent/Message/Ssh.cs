c923368dc5d114fcf5b5a7e0aa98ee1a4f0cc540
mihaibuzgau <mihai.buzgau@uhurusoftware.com>
2012-11-01 16:43:32 +0200

    fix for SSH
    Change-Id: I2bc7373cc18b49cf0b68d7975c7afb334f2078cc

--- src/Uhuru.BOSH.Agent/Message/Ssh.cs
+++ src/Uhuru.BOSH.Agent/Message/Ssh.cs
@@ -6,103 +6,134 @@
 
 namespace Uhuru.BOSH.Agent.Message
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using Uhuru.BOSH.Agent.Objects;
-    using Uhuru.Utilities;
-    using Newtonsoft.Json;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Uhuru.BOSH.Agent.Objects;
+	using Uhuru.Utilities;
+	using Newtonsoft.Json;
+	using System.Globalization;
+	using Microsoft.Win32;
+	using System.IO;
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Ssh", Justification = "FxCop Bug")]
+	public class Ssh : IMessage 
+	{
+		/// <summary>
+		/// Determines whether the message [is long running].
+		/// </summary>
+		/// <returns>
+		///   <c>true</c> if [is long running]; otherwise, <c>false</c>.
+		/// </returns>
+		public bool IsLongRunning()
+		{
+		   return false;
+		}
 
-    /// <summary>
-    /// TODO: Update summary.
-    /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Ssh", Justification = "FxCop Bug")]
-    public class Ssh : IMessage 
-    {
-        public bool IsLongRunning()
-        {
-           return false;
-        }
+		/// <summary>
+		/// Processes the specified args.
+		/// </summary>
+		/// <param name="args">The args.</param>
+		/// <returns></returns>
+		public object Process(dynamic args)
+		{
+			string sshType = args[0].Value.ToString();
 
-        public object Process(dynamic args)
-        {
-            string sshType = args[0].Value.ToString();
+			switch (sshType)
+			{
+				case "setup":
+					return SetupSsh(args[1]);
+				case "cleanup":
+					return CleanupSsh(args[1]);
+				default:
+					break;
+			}
 
-            switch (sshType)
-            {
-                case "setup":
-                    return SetupSsh(args[1]);
-                case "cleanup":
-                    return CleanupSsh(args[1]);
-                default:
-                    break;
-            }
+			return string.Empty;
 
-            return string.Empty;
+		}
 
-        }
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		private static object SetupSsh(dynamic parameters)
+		{
+			
+			string userName = parameters["user"].Value;
+			 
+			//Needed to enforce windows password rules 
+			string password = string.Format(CultureInfo.InvariantCulture, "{0}!", parameters["password"].Value);
+			SaveSaltInFile(userName, password.Substring(0, 2));
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        private static object SetupSsh(dynamic parameters)
-        {
-            
-            string userName = parameters["user"].Value;
-            string password = parameters["password"].Value;
-            Logger.Info("Setting up SSH with user:" + userName +" and password: " + password);
+			Logger.Info("Setting up SSH with user:" + userName +" and password: " + password);
 
-            SshResult sshResult = new SshResult();
-            sshResult.Command = "setup";
-            try
-            {
-                Uhuru.Utilities.WindowsVCAPUsers.CreateUser(userName, password);
-                sshResult.Status = "success";
-                Logger.Info("Created user for SSH");
-                SshdMonitor.StartSshd();
+			SshResult sshResult = new SshResult();
+			sshResult.Command = "setup";
+			try
+			{
+				Uhuru.Utilities.WindowsVCAPUsers.CreateUser(userName, password);
+				Uhuru.Utilities.WindowsUsersAndGroups.AddUserToGroup(userName, "Administrators");
+				sshResult.Status = "success";
+				Logger.Info("Created user for SSH");
+				SshdMonitor.StartSshd();
 
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("Failed to create user " + ex.ToString());
-                sshResult.Status = "failed";
-            }
-            sshResult.IP = Config.DefaultIP;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error("Failed to create user " + ex.ToString());
+				sshResult.Status = "failed";
+			}
+			sshResult.IP = Config.DefaultIP;
 
-            return sshResult;
-        }
+			return sshResult;
+		}
 
-        /// <summary>
-        /// Clean the SSH.
-        /// </summary>
-        /// <param name="parm">User parameters.</param>
-        /// <returns></returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        private static object CleanupSsh(dynamic parm)
-        {
-            string userRegex = parm["user_regex"].Value;
-            string userName = userRegex.Remove(0, 1);
-            userName = userName.Remove(userName.Length - 1, 1);
-            SshResult sshResult = new SshResult();
-            sshResult.Command = "cleanup";
-            Logger.Info("Cleaning up SSH");
-
-            try
-            {
-                WindowsVCAPUsers.DeleteUser(userName);
-                sshResult.Status = "success";
-                Logger.Info("Deleted user for SSH");
-                SshdMonitor.StopSshd();
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("Failed to delete user " + ex.ToString());
-                sshResult.Status = "failed";
-            }
-            
-            sshResult.IP = null;            
-            return sshResult;
-        }
+		private static void SaveSaltInFile(string user, string salt)
+		{
+			string saltDirPath = Path.Combine(Config.BaseDir, "bosh", "salt");
+			if (!Directory.Exists(saltDirPath))
+			{
+				Directory.CreateDirectory(saltDirPath);
+			}
+			File.WriteAllText(Path.Combine(saltDirPath, user + ".salt"), salt);
+		}
 
 
-    }
+		/// <summary>
+		/// Clean the SSH.
+		/// </summary>
+		/// <param name="parm">User parameters.</param>
+		/// <returns></returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		private static object CleanupSsh(dynamic parm)
+		{
+			string userRegex = parm["user_regex"].Value;
+			string userName = userRegex.Remove(0, 1);
+			userName = userName.Remove(userName.Length - 1, 1);
+			SshResult sshResult = new SshResult();
+			sshResult.Command = "cleanup";
+			Logger.Info("Cleaning up SSH");
+
+			try
+			{
+				WindowsVCAPUsers.DeleteUser(userName);
+				File.Delete(Path.Combine(Config.BaseDir, "bosh", "salt", userName + ".salt"));
+				Logger.Info("Deleted salt file");
+				sshResult.Status = "success";
+				Logger.Info("Deleted user for SSH");
+				SshdMonitor.StopSshd();
+			}
+			catch (Exception ex)
+			{
+				Logger.Error("Failed to delete user " + ex.ToString());
+				sshResult.Status = "failed";
+			}
+			
+			sshResult.IP = null;            
+			return sshResult;
+		}
+
+
+	}
 }
