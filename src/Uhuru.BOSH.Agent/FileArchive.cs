836f0029c515575339b26ef93ea7002b1dea0a12
florindragos <florind@uhurusoftware.com>
2013-09-26 16:35:59 +0300

    Changed zip library
    Change-Id: I3b58089eb422083049292ec760ac8e9b4708535b

--- src/Uhuru.BOSH.Agent/FileArchive.cs
+++ src/Uhuru.BOSH.Agent/FileArchive.cs
@@ -12,7 +12,8 @@ namespace Uhuru.BOSH.Agent
     using System.Text;
     using System.IO;
     using System.Reflection;
-    using SevenZip;
+    using ICSharpCode.SharpZipLib.Tar;
+    using ICSharpCode.SharpZipLib.GZip;
 
     /// <summary>
     /// TODO: Update summary.
@@ -20,27 +21,56 @@ namespace Uhuru.BOSH.Agent
     public static class FileArchive
     {
         /// <summary>
-        /// The lock for SevenZipSharp initialization
-        /// </summary>
-        private static readonly object zlibLock = new object();
-
-        /// <summary>
-        /// Flag if the SevenZipShparp library as initalized.
-        /// </summary>
-        private static bool zlibInitalized = false;
-
-        /// <summary>
-        /// Compresses a file using the .zip format.
+        /// Compresses a file using the .tgz format.
         /// </summary>
         /// <param name="sourceDir">The directory to compress.</param>
         /// <param name="fileName">The name of the archive to be created.</param>
         public static void ZipFile(string sourceDir, string fileName)
         {
-            SetupZlib();
+            using (Stream outStream = File.Create(fileName))
+            {
+                using (Stream gzoStream = new GZipOutputStream(outStream))
+                {
+                    using (TarArchive tarArchive = TarArchive.CreateOutputTarArchive(gzoStream))
+                    {
+                        // Note that the RootPath is currently case sensitive and must be forward slashes e.g. "c:/temp"
+                        // and must not end with a slash, otherwise cuts off first char of filename
+                        // This is scheduled for fix in next release
+                        tarArchive.RootPath = sourceDir.Replace('\\', '/');
+                        if (tarArchive.RootPath.EndsWith("/"))
+                            tarArchive.RootPath = tarArchive.RootPath.Remove(tarArchive.RootPath.Length - 1);
 
-            SevenZipCompressor compressor = new SevenZipCompressor();
-            compressor.ArchiveFormat = OutArchiveFormat.Zip;
-            compressor.CompressDirectory(sourceDir, fileName);
+                        AddDirectoryFilesToTar(tarArchive, sourceDir, true);
+                    }
+                }
+            }
+        }
+
+
+        private static void AddDirectoryFilesToTar(TarArchive tarArchive, string sourceDirectory, bool recurse)
+        {
+
+            // Optionally, write an entry for the directory itself.
+            // Specify false for recursion here if we will add the directory's files individually.
+            //
+            TarEntry tarEntry = TarEntry.CreateEntryFromFile(sourceDirectory);
+            tarArchive.WriteEntry(tarEntry, false);
+
+            // Write each file to the tar.
+            //
+            string[] filenames = Directory.GetFiles(sourceDirectory);
+            foreach (string filename in filenames)
+            {
+                tarEntry = TarEntry.CreateEntryFromFile(filename);
+                tarArchive.WriteEntry(tarEntry, true);
+            }
+
+            if (recurse)
+            {
+                string[] directories = Directory.GetDirectories(sourceDirectory);
+                foreach (string directory in directories)
+                    AddDirectoryFilesToTar(tarArchive, directory, recurse);
+            }
         }
 
         /// <summary>
@@ -50,80 +80,15 @@ namespace Uhuru.BOSH.Agent
         /// <param name="zipFile">The file to extract data from.</param>
         public static void UnzipFile(string targetDir, string zipFile)
         {
-            SetupZlib();
-
-            using (SevenZipExtractor extractor = new SevenZipExtractor(zipFile))
+            using (Stream inStream = File.OpenRead(zipFile))
             {
-                extractor.ExtractArchive(targetDir);
-            }
-        }
-
-        /// <summary>
-        /// Compresses a file using the tar and zip format.
-        /// </summary>
-        /// <param name="sourceDir">The source dir.</param>
-        /// <param name="fileName">Name of the archive file.</param>
-        public static void TarAndZipFile(string sourceDir, string fileName)
-        {
-            FileArchive.SetupZlib();
-            string text = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.ChangeExtension(fileName, ".tar"));
-            SevenZipCompressor sevenZipCompressor = new SevenZipCompressor();
-            sevenZipCompressor.ArchiveFormat = OutArchiveFormat.Tar;
-            sevenZipCompressor.CompressDirectory(sourceDir, text);
-            sevenZipCompressor.ArchiveFormat = OutArchiveFormat.Zip;
-            sevenZipCompressor.CompressFiles(fileName, new string[]
-			{
-				text
-			});
-            System.IO.File.Delete(text);
-        }
-
-        /// <summary>
-        /// Setups the zlib library; gets the proper 32 or 64 bit library as a stream from a resource, and loads it.
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Zlib", Justification = "Zlib is a spelled correctly")]
-        public static void SetupZlib()
-        {
-            if (zlibInitalized)
-            {
-                return;
-            }
-
-            lock (zlibLock)
-            {
-                if (zlibInitalized)
+                using (Stream gzipStream = new GZipInputStream(inStream))
                 {
-                    return;
-
+                    using (TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream))
+                    {                        
+                        tarArchive.ExtractContents(targetDir);
+                    }
                 }
-
-                Stream stream = null;
-                Assembly asm = Assembly.GetExecutingAssembly();
-                string libraryPath = string.Empty;
-
-                if (IntPtr.Size == 8)
-                {
-                    libraryPath = Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof(SevenZipExtractor)).Location), @"7z64.dll");
-                    stream = asm.GetManifestResourceStream("Uhuru.BOSH.Agent.lib.7z64.dll");
-                }
-                else
-                {
-                    libraryPath = Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof(SevenZipExtractor)).Location), @"7z86.dll");
-                    stream = asm.GetManifestResourceStream("Uhuru.BOSH.Agent.lib.7z86.dll");
-                }
-
-                if (!File.Exists(libraryPath))
-                {
-                    byte[] myAssembly = new byte[stream.Length];
-                    stream.Read(myAssembly, 0, (int)stream.Length);
-                    File.WriteAllBytes(libraryPath, myAssembly);
-                    stream.Close();
-                }
-
-                SevenZipCompressor.SetLibraryPath(libraryPath);
-                SevenZipExtractor.SetLibraryPath(libraryPath);
-
-                zlibInitalized = true;
             }
         }
     }
